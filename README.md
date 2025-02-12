28e6b38c044044b3e2872a990c6d9e04d44fed49
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2025-02-12 02:47:01 +0100

    in progress

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Pour cela on va utiliser différentes librairies python ainsi que des données o
 - Seaborn est une lib python de visualisation de données basée sur matplotlib.
 - Plotly est une lib python de visualisation de données interactive.
 - Pandas est une lib python de manipulation de données.
+
+# Seaborn
+- pairplot permet de visualiser les relations entre les variables de notre dataframe.
+![pairplot](pairplot.png)
+
+- displot permet de visualiser la distribution d'une variable.
+![displot](displot.png)
+
+- boxplot permet de visualiser la distribution d'une variable en fonction d'une autre variable.
+![boxplot](boxplot.png)
+
+- jointplot permet de visualiser la distribution de deux variables.
+![jointplot](jointplot.png)
+
+- heatmap permet de visualiser la corrélation entre les variables. C'est mon préféré.
+![heatmap](heatmap.png)
+
+# Plotly
+En plotly, on peut créer des figures avec des listes de lignes appelées traces.
--- app.py
+++ app.py
@@ -11,8 +11,73 @@ import plotly
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-# pd.set_option("precision", 2) 
-pd.options.display.float_format = '{:.2f}'.format 
+pd.set_option("display.precision", 2) # 2 ch après la virgule 
+pd.options.display.float_format = '{:.2f}'.format # force l'affichage de 2 ch après la virgule
 
 df = pd.read_csv('bank-additional/bank-additional-full.csv', sep=';')
-print(df.head(5))
+# print(df.head(5))
+
+d = {"no": 0, "yes": 1}
+df["y"] = df["y"].map(d) # remplace les valeurs de la colonne y par 0 et 1
+
+# print(df.columns) affiche les colonnes du dataframe
+# print(df.shape) affiche le nombre de lignes et de colonnes du dataframe
+
+
+### Créer des histogrammes avec matplotlib
+df["age"].hist()
+plt.title('Distribution of Age')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+
+df[["age", "marital"]].groupby(
+    "marital"
+).mean().plot(kind="bar", rot=45); # affiche la moyenne de l'age par statut marital
+
+df[["age", "job"]].groupby(
+    "job"
+).mean().plot(kind="bar", rot=45); # affiche la moyenne de l'age par job
+
+
+### Créer des histogrammes avec seaborn
+sns.pairplot(
+    df[["age","duration", "marital"]]
+);
+sns.displot(df.age); # affiche la distribution de l'age
+
+sns.jointplot(x="age", y="duration", data=df, kind="scatter") # affiche un nuage de points
+
+top_jobs = (df.job.value_counts().sort_values(ascending=False).head(10).index.values)
+sns.boxplot(y="job", x="age", data=df[df.job.isin(top_jobs)], orient="h") 
+'''
+df.job.value_counts() compte le nombre de fois que chaque job apparait
+puis on trie les jobs par ordre décroissant, en gardant les 10 premiers
+.index.values permet de récupérer les noms des jobs
+'''
+
+job_marital_y = (df.pivot_table(
+    index="job", columns = "marital", values="y", aggfunc="sum")
+)
+sns.heatmap(job_marital_y, annot=True, fmt="d", linewidths=0.5);
+plt.show()
+
+
+### Créer des histogrammes avec plotly
+age_df = (
+    df.groupby("age")[["y"]]
+    .sum().join(df.groupby("age")[["y"]].count(), rsuffix='_count')
+)
+age_df.columns = ["Attracted", "Total Number"]
+'''
+groupby("age")[["y"]] : groupe les données par age et sélectionne la colonne y.
+.join() permet de joindre les deux dataframes
+
+Ainsi, cette ligne permet de compter le nombre de personnes attirées par l'offre par age.
+'''
+trace0 = go.Scatter(x=age_df.index, y=age_df["Attracted"],name="Attracted")
+trace1 = go.Scatter(x=age_df.index, y=age_df["Total Number"],name="Total Number")
+
+data = [trace0, trace1]
+layout = {"title" : "Statistics of Attracted People by Age"}
+fig = go.Figure(data=data, layout=layout)
+iplot(fig)
